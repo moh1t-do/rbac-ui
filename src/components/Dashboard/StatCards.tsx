3dfbe4ec40cc0c973f37329bc4215e833b5df165
Mohit <mohitd2288@gmail.com>
2024-11-24 20:08:15 +0530

    feat: simplify StatCards

--- src/components/Dashboard/StatCards.tsx
+++ src/components/Dashboard/StatCards.tsx
@@ -1,21 +1,13 @@
-import { FiTrendingDown, FiTrendingUp } from "react-icons/fi";
-
 export const StatCards = () => {
   return (
     <>
       <Card
         title="Total Users"
         value="20"
-        pillText="2.75%"
-        trend="up"
-        period="From Jan 1st - Jul 31st"
       />
       <Card
         title="Total Roles"
         value="3"
-        pillText="1.01%"
-        trend="down"
-        period="From Jan 1st - Jul 31st"
       />
     </>
   );
@@ -24,15 +16,9 @@ export const StatCards = () => {
 const Card = ({
   title,
   value,
-  pillText,
-  trend,
-  period,
 }: {
   title: string;
   value: string;
-  pillText: string;
-  trend: "up" | "down";
-  period: string;
 }) => {
   return (
     <div className="col-span-6 p-4 rounded border border-stone-300">
@@ -41,19 +27,7 @@ const Card = ({
           <h3 className="text-stone-500 mb-2 text-sm">{title}</h3>
           <p className="text-3xl font-semibold">{value}</p>
         </div>
-
-        <span
-          className={`text-xs flex items-center gap-1 font-medium px-2 py-1 rounded ${
-            trend === "up"
-              ? "bg-green-100 text-green-700"
-              : "bg-red-100 text-red-700"
-          }`}
-        >
-          {trend === "up" ? <FiTrendingUp /> : <FiTrendingDown />} {pillText}
-        </span>
       </div>
-
-      <p className="text-xs text-stone-500">{period}</p>
     </div>
   );
 };
